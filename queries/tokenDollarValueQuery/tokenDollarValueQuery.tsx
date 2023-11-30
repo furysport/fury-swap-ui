52d1005470a4122f02dcd452ff6777f960579e22
Foxcraftfury <foxcraft.fury@protonmail.com>
2023-12-01 03:08:48 +0530

    fix price service

---
+++ queries/tokenDollarValueQuery/tokenDollarValueQuery.tsx
@@ -0,0 +1,32 @@
+import { TokenInfo } from 'queries/usePoolsListQuery'
+
+import { fetchDollarPriceByTokenIds, fetchDollarPriceFromFurySwapPool } from './fetchDollarPriceByTokenIds'
+import { pricingServiceIsDownAlert } from './pricingServiceIsDownAlert'
+
+export async function tokenDollarValueQuery(tokenIds: Array<TokenInfo['id']>) {
+  if (!tokenIds?.length) {
+    throw new Error('Provide token ids in order to query their price')
+  }
+
+  try {
+    let prices = await fetchDollarPriceByTokenIds(tokenIds)
+
+    // todo: usd prices for fury tokens are hardcoded for test purpose since they are not registered on coingecko
+    if (tokenIds.findIndex((tokenId) => tokenId === 'fury') != -1) {
+      let fury_price = await fetchDollarPriceFromFurySwapPool('fury');
+      prices = { ...prices, ...fury_price }
+    }
+
+    if (tokenIds.findIndex((tokenId) => tokenId === 'fury-xfury') != -1) {
+      let xfury_price = await fetchDollarPriceFromFurySwapPool('fury-xfury');
+      prices = { ...prices, ...xfury_price }
+    }
+
+    // console.log(prices)
+    return tokenIds.map((id): number => prices[id]?.usd || 0)
+  } catch (e) {
+    pricingServiceIsDownAlert()
+
+    throw e
+  }
+}
---
+++ services/pool.ts
@@ -0,0 +1,43 @@
+import poolData from 'public/pools_list.mainnet.json';
+import rpcConfig from 'public/chain_info.mainnet.json';
+import { CosmWasmClient, HttpEndpoint } from "@cosmjs/cosmwasm-stargate";
+
+const STABLECOIN_POOLS = ['FURY-USK'];
+
+export async function getPoolRatio(pool_id: string): Promise<number> {
+    const pool = poolData.pools.find(p => p.pool_id == pool_id);
+    if (!pool) {
+        return 0;
+    }
+
+    // Extracting the rpc endpoint from the rpcConfig object
+    const rpc: HttpEndpoint = { url: rpcConfig.rpc, headers: {} }; // You can update headers if they are available in rpcConfig
+
+    let client = await CosmWasmClient.connect(rpc);
+    const swap_info = await client.queryContractSmart(pool.swap_address, { info: {} });
+    client.disconnect();
+
+    if (swap_info.token1_reserve != 0) {
+        const t = Number(swap_info.token2_reserve) / Number(swap_info.token1_reserve);
+        return Number(t.toPrecision(6));
+    }
+    return 0;
+}
+
+export async function get_FURY_USD_Price(): Promise<number> {
+    let sum = 0
+    for (let i = 0;i < STABLECOIN_POOLS.length;i++) {
+        sum += await getPoolRatio(STABLECOIN_POOLS[i])
+    }
+    return sum / STABLECOIN_POOLS.length
+}
+
+export async function get_xFURY_USD_Price(): Promise<number> {
+    const ratio = await getPoolRatio('FURY-xFURY')
+    if (ratio == 0) {
+        return 0
+    }
+    const fury_usd_price = await get_FURY_USD_Price()
+    const xfury_price = (fury_usd_price / ratio).toPrecision(6)
+    return Number(xfury_price)
+}
